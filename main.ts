9b0a8d85b9281df75bc1647dec5dd5d2a6413e1f
Felix Wotschofsky <felix@wotschofsky.com>
2024-12-10 17:55:13 +0100

    Make user aliases permanent 👻

--- main.ts
+++ main.ts
@@ -24,12 +24,19 @@ async function resolveUserTags(text: string): Promise<string> {
   const userTagRegex = /<@([A-Z0-9]+)>/g;
   const userTags = text.match(userTagRegex) || [];
   for (const tag of userTags) {
-    const randomName = uniqueNamesGenerator({
-      dictionaries: [adjectives, colors, animals],
-      style: 'capital',
-      separator: ' ',
-    });
-    text = text.replace(tag, randomName);
+    const userId = tag.match(/<@([A-Z0-9]+)>/)?.[1];
+    if (!userId) continue;
+
+    let userAlias = await redis.get(`user_alias:${userId}`);
+    if (!userAlias) {
+      userAlias = uniqueNamesGenerator({
+        dictionaries: [adjectives, colors, animals],
+        style: 'capital',
+        separator: ' ',
+      });
+      await redis.set(`user_alias:${userId}`, userAlias);
+    }
+    text = text.replace(tag, userAlias);
   }
   return text;
 }
